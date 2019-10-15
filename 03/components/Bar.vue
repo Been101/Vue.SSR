a1c6f4393aa8b184876f427fdb6d21cbc4f9412c
been <1043609130@qq.com>
2019-10-15 22:40:45 +0800

    add component

---
+++ 03/components/Bar.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    Bar component {{msg}}
+  </div>
+</template>
+
+<script>
+function fetchData(rootData) {
+  rootData.bar.name = "fetch data from db";
+}
+export default {
+  asyncData: fetchData,
+  data() {
+    return {};
+  },
+  mounted() {
+    fetchData(this.$root.rootData);
+  },
+  computed: {
+    msg() {
+      return this.$root.rootData.bar.name;
+    }
+  }
+};
+</script>
+
+<style>
+</style>
