beed858ee5957858f6974f7489bfdd09023e2016
been <1043609130@qq.com>
2019-10-15 22:41:31 +0800

    03 config

---
+++ 03/config/webpack.base.config.js
@@ -0,0 +1,31 @@
+const path = require('path')
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+module.exports = {
+    mode: 'development',
+    output: {
+        path: path.resolve(__dirname, '../dist'),
+        filename: '[name].bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader'
+            },
+            {
+                test: /\.js$/,
+                loader: 'babel-loader'
+            },
+            {
+                test: /\.css$/,
+                loader: ['vue-style-loader', 'css-loader', 'postcss-loader']
+            }
+        ]
+    },
+    plugins:[
+        new VueLoaderPlugin(),
+        new ExtractTextPlugin("styles.css")
+    ]
+}
