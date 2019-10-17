99ae5d41ab1ebda881e2eed72c0a4055dae22537
been <1043609130@qq.com>
2019-10-17 23:15:28 +0800

    04 compoent

---
+++ 04/components/Bar.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    Bar component {{msg}}
+  </div>
+</template>
+
+<script>
+const rootData = {
+  bar: {
+    name: ""
+  }
+};
+function fetchData() {
+  return new Promise((resolve, reject) => {
+    rootData.bar.name = "fetch data from db";
+    resolve("fetch data from db");
+  });
+}
+export default {
+  asyncData: fetchData,
+  data() {
+    return {};
+  },
+  mounted() {
+    fetchData().then(res => {
+      rootData.bar.name = res;
+    });
+  },
+  computed: {
+    msg() {
+      return rootData.bar.name;
+    }
+  }
+};
+</script>
+
+<style>
+</style>
