3ed1344b5292995517321dce93881a0148203a9b
been <1043609130@qq.com>
2019-10-17 23:15:56 +0800

    store factory

---
+++ 04/store/store.js
@@ -0,0 +1,38 @@
+import Vue from 'Vue'
+import Vuex from 'Vue'
+
+Vue.use(Vuex)
+
+const fetchBar = function () {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('bar 组件返回 ajax 数据')
+    }, 1000)
+  })
+}
+
+export function createStore() {
+  const store = new Vuex.Store({
+    state: {
+      bar: ''
+    },
+
+    mutations: {
+      'SET_BAR'(state, data) {
+        state.bar = data
+      }
+    },
+
+    actions: {
+      fetchBar({ commit }) {
+        return fetchBar().then(data => {
+          commit('SET_BAR', data)
+        }).catch(err => {
+          console.log(err)
+        })
+      }
+    }
+  })
+
+  return store
+}
