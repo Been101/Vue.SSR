02daa9776f505ea9b640277ee62fd913c507ccc8
been <1043609130@qq.com>
2019-10-19 22:44:43 +0800

    server.js

--- server.js
+++ server.js
@@ -10,19 +10,22 @@ const project = argv[2]
 
 const bundle = fs.readFileSync(path.resolve(__dirname, `./${project}/dist/server.bundle.js`), 'utf-8');
 
-const renderer = require('vue-server-renderer').createBundleRenderer(bundle, {
+const vueServerRenderer = require('vue-server-renderer')
+
+const renderer = vueServerRenderer.createBundleRenderer(bundle, {
   template: fs.readFileSync(path.resolve(__dirname, `./${project}/dist/index.ssr.html`), 'utf-8')
 });
-
 // 后端Server
 backendRouter.get('/index', async (ctx, next) => {
   // 这里用 renderToString 的 promise 返回的 html 有问题，没有样式
   const html = await new Promise((resolve, reject) => {
-    renderer.renderToString((err, html) => {
+
+    renderer.renderToString((err, htmlStr) => {
       if (err) {
+        console.log('err--', err)
         reject(err)
       } else {
-        resolve(html)
+        resolve(htmlStr)
       }
     });
   })
@@ -32,7 +35,7 @@ backendRouter.get('/index', async (ctx, next) => {
   ctx.body = html
 });
 
-backendApp.use(serve(path.resolve(__dirname, `./${project}/dist`)));
+backendApp.use(serve(path.resolve(__dirname, `./${project}/dist`), { index: false }));
 
 backendApp
   .use(backendRouter.routes())
