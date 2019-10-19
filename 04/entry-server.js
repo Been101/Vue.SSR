923a202722d26c51d22a1d845a0840c7a37a41bb
been <1043609130@qq.com>
2019-10-19 22:45:51 +0800

    export default

--- 04/entry-server.js
+++ 04/entry-server.js
@@ -1,19 +1,20 @@
-import { createApp } from './app.js';
+import createApp from './app.js';
 
 export default context => {
     return new Promise((resolve, reject) => {
-        const { app, rootData, App } = createApp()
+        const { app, store, App } = createApp()
         const components = App.components
         console.log(components, '************')
         const asyncDataPromiseFns = []
+
         Object.values(components).forEach(component => {
             if (component.asyncData) {
-                asyncDataPromiseFns.push(component.asyncData())
+                asyncDataPromiseFns.push(component.asyncData(store))
             }
-        });
+        })
 
-        Promise.all(asyncDataPromiseFns).then(result => {
-            context.state = result
+        Promise.all(asyncDataPromiseFns).then(() => {
+            context.state = store.state
             resolve(app)
         }, reject)
     })
