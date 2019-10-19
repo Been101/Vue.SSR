59efa6311fdbf50e4a254ccd7eae746a470ca115
been <1043609130@qq.com>
2019-10-20 00:24:21 +0800

    返回的html 正常被激活

--- 02/components/Bar.vue
+++ 02/components/Bar.vue
@@ -1,15 +1,32 @@
 <template>
-  <div>
-      Bar component
+  <div class="bar">
+    Bar component
+    <p>{{msg}}</p>
+    <button @click='fetch'>Fetch Data</button>
   </div>
 </template>
 
 <script>
 export default {
-
-}
+  data() {
+    return {
+      msg: ""
+    };
+  },
+  methods: {
+    fetch() {
+      new Promise(() => {
+        setTimeout(() => {
+          this.msg = "fetch from server";
+        }, 1000);
+      });
+    }
+  }
+};
 </script>
 
-<style>
-
+<style scoped>
+.bar {
+  background-color: yellow;
+}
 </style>
