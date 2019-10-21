b2455e7ad2eb9a1f1d22bd8d9ff2f777190aed03
been <1043609130@qq.com>
2019-10-21 22:57:43 +0800

    component

---
+++ 05/components/Bar.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    Bar component {{msg}}
+  </div>
+</template>
+
+<script>
+function fetchData(store) {
+  store.dispatch("fetchBar");
+}
+export default {
+  asyncData: fetchData,
+  data() {
+    return {};
+  },
+  mounted() {
+    const store = this.$store;
+    fetchData(store);
+  },
+  computed: {
+    msg() {
+      return this.$store.state.bar;
+    }
+  }
+};
+</script>
+
+<style>
+</style>
