d2c5f7036893b385670c65bd077056e6f2d4dd54
been <1043609130@qq.com>
2019-11-04 23:12:12 +0800

    server.js

--- server.js
+++ server.js
@@ -9,31 +9,23 @@ const argv = process.argv
 const project = argv[2]
 
 if (project !== '01') {
-  const bundleJson = require(`./${project}/dist/vue-ssr-server-bundle.json`);
+  let bundle
+  if (project === '02') {
+    bundle = require(`./${project}/dist/server.bundle.js`);
+  } else {
+    bundle = require(`./${project}/dist/vue-ssr-server-bundle.json`);
+  }
 
   const vueServerRenderer = require('vue-server-renderer')
 
-  // 配置惰性路由后， bundle 中不包含惰性路由组件的js文件, 会报错，  ?
-  const renderer = vueServerRenderer.createBundleRenderer(bundleJson, {
+  // 配置惰性路由后， server bundle 中不包含惰性路由组件的js文件, 会报错，  ?
+  const renderer = vueServerRenderer.createBundleRenderer(bundle, {
     template: fs.readFileSync(path.resolve(__dirname, `./${project}/dist/index.ssr.html`), 'utf-8')
   });
 
   // 后端Server
   backendRouter.get('*', async (ctx, next) => {
-    // if (ctx.url !== '/favicon.ico') {
     const html = await renderer.renderToString(ctx)
-
-    // 为什么下面用异步的方法就不行， 看koa
-    // .then(html => {
-    //   console.log(html, '-------------')
-    //   ctx.type = 'html'
-    //   ctx.status = 200
-    //   ctx.body = html
-    // })
-    // , (err, html) => {
-
-    // })
-    // }
     ctx.body = html
   });
 }
