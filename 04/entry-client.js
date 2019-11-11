c60e6ca9cef862dd86d84830ba2f6749f85cfd4c
been <1043609130@qq.com>
2019-11-11 22:39:03 +0800

    get data from __INITIAL_STATE__

--- 04/entry-client.js
+++ 04/entry-client.js
@@ -1,8 +1,12 @@
 import { createApp } from './app.js'
-
-const { app, router } = createApp()
+const { app, router, store } = createApp()
 
 router.onReady(() => {
+
+  if (window.__INITIAL_STATE__) {
+    store.replaceState(window.__INITIAL_STATE__)
+  }
   // 这里假定 App.vue 模板中根元素具有 `id="app"`
   app.$mount('#app')
 })
+
